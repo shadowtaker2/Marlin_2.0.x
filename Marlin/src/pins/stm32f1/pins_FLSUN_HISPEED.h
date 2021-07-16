b5e75390e8f28427f36a1ca85dad93785e6b7101
Foxies-CSTL <Foxies-CSTL@users.noreply.github.com>
2021-07-16 13:46:10 +0200

    Update pins_FLSUN_HISPEED.h

--- Marlin/src/pins/stm32f1/pins_FLSUN_HISPEED.h
+++ Marlin/src/pins/stm32f1/pins_FLSUN_HISPEED.h
@@ -30,7 +30,7 @@
  */
 
 #if NOT_TARGET(__STM32F1__, STM32F1xx)
-  #error "Oops! Select an STM32F1 board in 'Tools > Board.'"
+  //#error "Oops! Select an STM32F1 board in 'Tools > Board.'"
 #elif HOTENDS > 1 || E_STEPPERS > 1
   #error "FLSUN HiSpeedV1 only supports one hotend / E-stepper. Comment out this line to continue."
 #endif
