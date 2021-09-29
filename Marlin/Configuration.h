2156e7c9f3328a22eed598fe12cc3007bbbc2438
shadowtaker2 <91563435+shadowtaker2@users.noreply.github.com>
2021-09-29 03:11:39 +0100

    p

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -130,8 +130,7 @@
 //#define BLUETOOTH
 
 // Choose the name from boards.h that matches your setup
-  #ifdef SKR14T
-    #define BOARD_BTT_SKR_V1_4_TURBO
+ #define BOARD_BTT_SKR_V1_4_TURBO
   #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
